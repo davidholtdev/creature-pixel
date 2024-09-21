372d9f8c3177f822f2d9093260e602694e5477f6
David Holt <43420422+davidholtdev@users.noreply.github.com>
2024-09-21 18:26:52 +0100

    :sparkles: Implemented error page (#6)

---
+++ layouts/error.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="page-wrapper page-wrapper--error">
+    <main>
+      <slot></slot>
+    </main>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
