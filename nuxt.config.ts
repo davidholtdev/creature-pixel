2c1bf6acafd5beb61de814fc32e43d0e8beb87be
David Holt <david.holt.dev@hotmail.com>
2024-09-21 14:48:50 +0100

    Implemented web manifest

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,18 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   css: ["~/assets/scss/main.scss"],
+  app: {
+    head: {
+      link: [
+        { rel: "icon", type: "image/x-icon", href: "/icons/favicon.ico" },
+        { rel: "apple-touch-icon", sizes: "180x180", href: "/icons/apple-touch-icon.png" },
+        { rel: "icon", type: "image/png", sizes: "32x32", href: "/icons/favicon-32x32.png" },
+        { rel: "icon", type: "image/png", sizes: "16x16", href: "/icons/favicon-16x16.png" },
+        { rel: "manifest", href: "/site.webmanifest" },
+      ],
+      meta: [{ name: "theme-color", content: "#03D1C5" }],
+    },
+  },
   modules: ["@nuxt/image", "@nuxtjs/google-fonts"],
   googleFonts: {
     families: {
