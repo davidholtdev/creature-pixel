2b41fb3f1cca9f5e1e6a0c87c77c7cc9c92ccf16
David Holt <43420422+davidholtdev@users.noreply.github.com>
2024-09-26 19:20:22 +0100

    :hammer: Github pages configuration (#10)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,20 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import { getBaseUrl } from "./utils/helpers";
+
+const baseURL = getBaseUrl();
+
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   css: ["~/assets/scss/main.scss"],
   app: {
+    baseURL: baseURL,
     head: {
       link: [
-        { rel: "icon", type: "image/x-icon", href: "/icons/favicon.ico" },
-        { rel: "apple-touch-icon", sizes: "180x180", href: "/icons/apple-touch-icon.png" },
-        { rel: "icon", type: "image/png", sizes: "32x32", href: "/icons/favicon-32x32.png" },
-        { rel: "icon", type: "image/png", sizes: "16x16", href: "/icons/favicon-16x16.png" },
-        { rel: "manifest", href: "/site.webmanifest" },
+        { rel: "icon", type: "image/x-icon", href: `${baseURL}icons/favicon.ico` },
+        { rel: "apple-touch-icon", sizes: "180x180", href: `${baseURL}icons/apple-touch-icon.png` },
+        { rel: "icon", type: "image/png", sizes: "32x32", href: `${baseURL}icons/favicon-32x32.png` },
+        { rel: "icon", type: "image/png", sizes: "16x16", href: `${baseURL}icons/favicon-16x16.png` },
+        { rel: "manifest", href: `${baseURL}site.webmanifest` },
       ],
       meta: [{ name: "theme-color", content: "#03D1C5" }],
     },
@@ -35,23 +39,9 @@ export default defineNuxtConfig({
       xxl: 1400,
     },
   },
-  // app: {
-  //   pageTransition: {
-  //     name: "page",
-  //     mode: "out-in",
-  //   },
-  //   layoutTransition: {
-  //     name: "page",
-  //     mode: "out-in",
-  //   },
-  // },
-  // build: {
-  //   loaders: {
-  //     scss: {
-  //       implementation: require("sass"),
-  //     },
-  //   },
-  // },
+  nitro: {
+    preset: "github-pages",
+  },
   vite: {
     css: {
       preprocessorOptions: {
